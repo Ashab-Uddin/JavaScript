f93918f566b3f8547bfbc2004b8f1f29d391a953
Ashab Uddin <ashabuddin47@gmail.com>
2024-09-30 19:49:22 +0600

    learn some Math function

---
+++ js.ProblemPart-1/minNumber.js
@@ -0,0 +1,10 @@
+
+let num =Math.min(20,49,20,39,49,10,20,20,10,59);
+console.log(num);
+let max =Math.max(20,49,20,39,49,10,20,20,10,59);
+console.log(max);
+
+console.log(Math.round(3.8))//Math.round convert nearest int number;
+console.log(Math.floor(5.99999))//Math.floor convert in this number integer number;
+console.log(Math.ceil(34.42))//Math.ceil convert upper number;
+console.log(Math.abs(3-53))//abs convert negetive to posive;
