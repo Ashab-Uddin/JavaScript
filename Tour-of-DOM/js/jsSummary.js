fe6758660a40772529c02263d091fc9fad8afb25
Ashab Uddin <ashabuddin47@gmail.com>
2024-10-13 20:02:06 +0600

    add a input field and apply addEventListener

--- Tour-of-DOM/js/jsSummary.js
+++ Tour-of-DOM/js/jsSummary.js
@@ -6,3 +6,11 @@ document.getElementById('addEventLisener').addEventListener('click' ,function(){
 const handleStatus = document.getElementById('handale-status');
 handleStatus.innerText = 'Wow ! You are Succsessfully Completed by AddEventListener';
 })
+
+document.getElementById('btn-send').addEventListener('click',function(){
+    const inputTexts = document.getElementById('inputText');
+    const inputFieldText = inputTexts.value;
+    const p = document.getElementById('defaulText');
+    p.innerText = inputFieldText;
+    inputTexts.value = '';
+})
