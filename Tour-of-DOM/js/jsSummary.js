a381b1392fe1b9d8b5ec4940981304340eb9ca64
Ashab Uddin <ashabuddin47@gmail.com>
2024-10-13 19:06:45 +0600

    learn AddEventListener

---
+++ Tour-of-DOM/js/jsSummary.js
@@ -0,0 +1,8 @@
+function clickNow(){
+    const handleStatus = document.getElementById('handale-status');
+    handleStatus.innerText = 'WoW! You are Successfully Complelete';
+}
+document.getElementById('addEventLisener').addEventListener('click' ,function(){
+const handleStatus = document.getElementById('handale-status');
+handleStatus.innerText = 'Wow ! You are Succsessfully Completed by AddEventListener';
+})
---
+++ Tour-of-DOM/js/styleInJs.js
@@ -0,0 +1,8 @@
+const sections =document.querySelectorAll('section');
+for(const section of sections){
+    section.style.border = '2px solid blue ';
+    section.style.marginBottom = '5px';
+    section.style.borderRadius = '8px';
+    section.style.paddingLeft = '8px';
+    section.style.backgroundColor = 'lightgray'
+}
