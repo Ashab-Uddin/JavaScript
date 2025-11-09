a7d18d039a127d1a49ca060ffab3b18bc8194d95
Ashab Uddin <ashabuddin47@gmail.com>
2025-11-10 00:04:58 +0600

    learn array

---
+++ Array/number.js
@@ -0,0 +1,5 @@
+var num1= 20;
+var num2= "20";
+var num3 = parseInt(num2);
+// console.log(num1+num2)
+console.log(num1+num3);
---
+++ Array/shift.js
@@ -0,0 +1,6 @@
+var marks = [20,30,40,50];
+console.log(marks);
+marks.shift();
+console.log(marks);
+marks.unshift(100);
+console.log(marks);
